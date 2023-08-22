d0a8249219d333af0c5e677d089eea4fef26b868
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-22 09:36:20 -0400

    Add createCard function

---
+++ src/card.js
@@ -0,0 +1,9 @@
+const createCard = (id, question, answers, correctAnswer) => {
+  let card = {
+    id: id,
+    question: question,
+    answers: answers,
+    correctAnswer: correctAnswer
+  }
+  return card
+}
