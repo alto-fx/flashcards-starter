2cb4498deb92a13c00b9116ac49c2cc9c45d74c2
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-22 15:25:12 -0400

    Add test code to check if user guess is correct or incorrect

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,14 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess } = require('../src/card');
 
 describe('card', function() {
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(createCard).to.be.a('function');
   });
 
-  it.skip('should create a card and its properties', function() {
+  it('should create a card and its properties', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     
     expect(card.id).to.equal(1);
@@ -17,3 +17,21 @@ describe('card', function() {
     expect(card.correctAnswer).to.equal('object');
   });  
 });
+
+  describe("guess", () => {
+    // evaluate the guess. happy path if correct. sad path if incorrect
+    it("should tell user guess is correct", () => {
+      const card = createCard(1, "What allows you to define a set of related information using key-value pairs?",["object', 'array', 'function"], "object")
+      const userGuess = "object"
+      let guessResult = evaluateGuess(userGuess, card.correctAnswer)
+      expect(guessResult).to.equal("correct!")
+    })
+
+    it("should tell user guess is incorrect", () => {
+      const card = createCard(1, "What allows you to define a set of related information using key-value pairs?",["object', 'array', 'function"], "object")
+      const userGuess = "array" || "function"
+      let guessResult = evaluateGuess(userGuess, card.correctAnswer)
+      expect(guessResult).to.equal("incorrect")
+    })
+  });
+
