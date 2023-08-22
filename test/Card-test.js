94fdbeb4fc5ee04655e2fbaf3b66c0e284ae6078
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-22 18:27:06 -0400

    Add test for createRound function

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard, evaluateGuess, createDeck, countCards, } = require('../src/card');
+const { createCard, evaluateGuess, createDeck, countCards, createRound, } = require('../src/card');
 
 describe('card', function () {
   it('should be a function', function () {
@@ -54,6 +54,28 @@ describe("deck", () => {
   })
 });
 
+describe("round", () => {
+  let card1, card2, card3, deck, round;
+
+  beforeEach(() => {
+    card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = createDeck([card1, card2, card3])
+    round = createRound(deck)
+  });
+
+  it("should be a function", () => {
+    expect(createRound).to.be.a("function")
+  })
+
+  it("should create a round", () => {
+    expect(round.deck).to.equal(deck)
+    expect(round.currentCard).to.equal(deck.cards[0])
+    expect(round.turns).to.equal(0)
+    expect(round.incorrectGuesses).to.deep.equal([])
+  })
+});
 
 
 
