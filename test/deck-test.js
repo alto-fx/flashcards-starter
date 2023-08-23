faa1019868d6b8a7b7d622820995732549b39ac2
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-23 09:41:18 -0400

    Fix createCard import to test file

--- test/deck-test.js
+++ test/deck-test.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const { createCard } = require('../src/card');
 const { createDeck, countCards, } = require('../src/deck');
-
 
 describe("deck", () => {
   let card1, card2, card3, deck;
