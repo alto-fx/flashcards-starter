abbf2960e58655c1f09a640a38f982350c459c51
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-24 18:26:21 -0400

    Add startGame function to begin game

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-// This is where your project starts.
-
 console.log('Your project is running...'); 
+
+const { prototypeData } = require('./src/data.js');
+const { createCard } = require('./src/card.js');
+const { createDeck } = require('./src/deck.js');
+const { createRound } = require('./src/round.js')
+const { printMessage, printQuestion } = require('./src/game.js');
+
+const startGame = () => {
+  const cards = prototypeData.map((card) => {
+    return createCard(card.id, card.question, card.answers, card.correctAnswer)
+  })
+  const deck = createDeck(cards)
+  const round = createRound(deck)
+
+  printMessage(deck)
+  printQuestion(round)
+}
+
+startGame();
+
